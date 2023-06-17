21ed0ac47a832605f52c536b1392e1e9cb2199b9
Shann Chongwattananukul <shanj3131@gmail.com>
2023-06-18 04:09:30 +0700

    small update

--- all_artifacts.py
+++ all_artifacts.py
@@ -1,5 +1,6 @@
 from qiime2 import Metadata, Artifact
 from skbio.tree import TreeNode
+from skbio.stats.distance import DistanceMatrix
 import pandas as pd
 metadata = pd.read_csv('./ds_metadata.tsv', sep='\t')
 id_key: dict = {
@@ -12,23 +13,44 @@ id_key: dict = {
     "ViS": "Villum station"
 }
 
+def to_distance(artifact_path: str):
+    return Artifact.load(artifact_path).view(DistanceMatrix)
+
+def to_df(artifact_path: str):
+    return Artifact.load(artifact_path).view(Metadata).to_dataframe()
+
 # Principle component analyses
-pcoaBC_2D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-2D_B_braycurtis.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaJ_2D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-2D_B_jaccard.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaUU_2D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-2D_BPhylo_unweighted_unifrac_FastTree.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaNU_2D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-2D_BPhylo_weighted_normalized_unifrac_FastTree.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaWU_2D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-2D_BPhylo_weighted_unifrac_FastTree.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaBC_3D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-3D_B_braycurtis.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaJ_3D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-3D_B_jaccard.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaUU_3D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-3D_BPhylo_unweighted_unifrac_FastTree.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaNU_3D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-3D_BPhylo_weighted_normalized_unifrac_FastTree.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
-pcoaWU_3D = Artifact.load('results/8-Analysis/Merged/Merged-PCOA-3D_BPhylo_weighted_unifrac_FastTree.qza').view(Metadata).to_dataframe().merge(metadata, left_index=True, right_on='sample-id')
+beta_metrics: dict = { "bc": "B_braycurtis", "ja": "B_jaccard",
+                    "uu": "BPhylo_unweighted_unifrac",
+                    "wn": "BPhylo_weighted_normalized_unifrac",
+                    "wu": "BPhylo_weighted_unifrac"}
+alpha_metrics: dict = {"sh": "shannon", "pi": "pielou_e",
+                    "si": "simpson", "se": "simpson_e"}
+alpha = {}
+for loc in id_key:
+    path = "results/7-Diversity"
+    alpha[loc] = {}
+    for name, metric in alpha_metrics.items():
+        alpha[loc][name] = Artifact.load(f"{path}/{loc}/{loc}-A_{metric}.qza").view(Metadata).to_dataframe()
+beta_dm = {}
+pcoa2D = {}
+pcoa3D = {}
+for name, metric in beta_metrics.items():
+        dm_path = "results/7-Diversity/Merged/Merged-"
+        pcoa_path = "results/8-Analysis/Merged/Merged-PCOA"
+        if not name in {"bc", "ja"}:
+            ext = "_FastTree"
+        else:
+            ext = ""
+        beta_dm[name] = to_distance(f"{dm_path}{metric}{ext}.qza")
+        pcoa2D[name] = to_df(f"{pcoa_path}-2D_{metric}{ext}.qza").merge(metadata, left_index=True, right_on="sample-id")
+        pcoa3D[name] = to_df(f"{pcoa_path}-3D_{metric}{ext}.qza").merge(metadata, left_index=True, right_on="sample-id")
 
 # Taxonomy
 #   BLAST
 blast = {}
 for loc in id_key:
-    blast_result = f'./results/3-Classified/{loc}-BLAST_ALL.qza'
+    blast_result = f'./results/3-Classified/{loc}-BLAST_All.qza'
     blast_top = f'./results/3-Classified/{loc}-BLAST_TopHits.qza'
     blast[loc] = [Artifact.load(r).view(Metadata).to_dataframe() for r in
                 (blast_result, blast_top)]
